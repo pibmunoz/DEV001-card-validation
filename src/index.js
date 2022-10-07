24f68bdcb518281a20464a66cedcfe6c41aa4a6c
pibmunoz <p6.munoz@gmail.com>
2022-10-06 22:33:56 -0400

    Maskify event fixed and final css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+//Mostrar y ocultar sección de Resumen de Compra
+document.getElementById("hiding-button").addEventListener("click", () => {
+    let productsDisplay = document.getElementById("products-cart");
+    if (productsDisplay.style.display === "none") {
+        productsDisplay.style.display = "block";
+      } else {
+        productsDisplay.style.display = "none";
+      }
+});
+
+// Invocar isValid con el click en submit + alert con maskify
+document.getElementById("submit").addEventListener("click", function() {
+  let creditCardNumber = document.getElementById("card-numbers").value;
+  if (validator.isValid(creditCardNumber)) {
+    return alert("¡PAGO REALIZADO CON ÉXITO! CON TARJETA N° " + validator.maskify(creditCardNumber));
+  } else {
+    return alert("INGRESA UN NÚMERO DE TARJETA VÁLIDO")
+  }
+  });
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,53 @@
 const validator = {
   // ...
-};
+  isValid: function(creditCardNumber) {
+    
+    let cardNumbers = Array.from(creditCardNumber).reverse();
 
+    // Separar datos según posición (par e inpar)
+    let oddNumber = cardNumbers.filter((element, index) => {
+        return ((index + 1) % 2 == 0);
+    });
+    let evenNumber = cardNumbers.filter((element, index) => {
+        return ((index + 1) % 2 !== 0);
+    });
+    // Doble de los números en posición par
+    let doubleOddNumber = oddNumber.map(function(x) {
+        return x * 2;
+    });
+    // Join Array, todo junto
+    let joinOddArray = doubleOddNumber.join("");
+    
+    // Crear nuevamente una Array y así separar los dígitos
+    let finalOddArray = Array.from(joinOddArray);
+    
+    // Concatena las dos Arrays
+    let concatResult = finalOddArray.concat(evenNumber);
+    
+    // Transformar array concatenada en número
+    let numbResult = concatResult.map(Number);
+    
+    // Sumar las dos arrays
+    let finalResult = numbResult.reduce(function(previousValue, currentValue) {
+        let finalArray = previousValue + currentValue;
+        return finalArray;
+    }, 0);
+    // Ver último dígito
+    let resultLastDigit = Math.floor(finalResult % 10);
+    if (resultLastDigit == 0 && creditCardNumber !== "") {
+      console.log("VÁLIDA");
+      return true;
+    } else if (resultLastDigit !== 0 && creditCardNumber !== "") {
+      console.log("INVÁLIDA");
+      return false;
+    }
+  },
+  maskify: function(creditCardNumber) {
+    if (creditCardNumber.length <= 5) {
+      return "#".repeat(creditCardNumber.length - 1) + creditCardNumber.slice(-4);
+    } else if (creditCardNumber.length > 5) {
+      return "#".repeat(creditCardNumber.length - 4) + creditCardNumber.slice(-4);
+  }
+}
+};
 export default validator;
